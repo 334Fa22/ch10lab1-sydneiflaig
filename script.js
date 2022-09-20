2795f8e2fb2176295669a2f367553acb91dc70c8
Sydnei Flaig <s539692@nwmissouri.edu>
2022-09-20 12:56:27 -0500

    ch. 10

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Student Name: Sydnei Flaig
+File Name: script.js
+Date: September 20, 2022
+*/
+
+//Hamburger menu function
+function hamburger(){
+var menu = document.getElementById("menu-links");
+if (menu.style.diplay === "block"){
+menu.style.display = "none";
+} else {
+  menu.style.display = "block";
+}
+}
